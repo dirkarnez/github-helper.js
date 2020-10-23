11fbc0350335934c7fc53f6e0252a697325e0b1d
Dirk Arnez <smalldirkalex@gmail.com>
2020-10-24 02:28:42 +0800

    - update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-// 
-let match = window.location.href.match("https://github.com/[^/]+/[^/]+/([^/]+)/[^/]+/?(.*)");
-console.log(match[1], match[2]);
+// ==UserScript==
+// @name         GitHub Helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/dirkarnez/*
+// @grant        none
+// ==/UserScript==
 
-// name
-document.getElementsByName("filename")[0].value
-
-// commit message
-document.getElementsByName("message")[1].value
+(function() {
+    'use strict';
+    let match = window.location.href.match("https://github.com/[^/]+/[^/]+/([^/]+)/[^/]+/?(.*)");
+    if (match[1] == "edit") {
+        document.getElementsByName("message")[1].value = `- update ${match[2]}`;
+    } else if (match[1] == "new") {
+        document.getElementsByName("filename")[0].addEventListener("change", function(e) {
+            document.getElementsByName("message")[1].value = `- add ${e.target.value}`;
+        });
+    }
+})();
